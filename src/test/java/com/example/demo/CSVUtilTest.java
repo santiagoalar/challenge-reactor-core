8ab350ce04a59023772b0e742670b94aa191e70f
Santiago Alarcon <santiago-alar@hotmail.com>
2021-06-28 12:02:41 -0500

    First commit

--- src/test/java/com/example/demo/CSVUtilTest.java
+++ src/test/java/com/example/demo/CSVUtilTest.java
@@ -63,6 +63,39 @@ public class CSVUtilTest {
         assert listFilter.block().size() == 322;
     }
 
+    @Test
+    void reactive_filtrarJugadoresMayoresA34DelColoColo(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> listFlux = Flux.fromStream(list.parallelStream()).cache();
+        Mono<Map<String, Collection<Player>>> listFilter = listFlux
+                .filter(player -> player.age >= 34)
+                .filter(player -> player.club.equals("Colo-Colo"))
+                .map(player -> {
+                    player.name = player.name.toUpperCase(Locale.ROOT);
+                    return player;
+                })
+                .buffer(100)
+                .flatMap(playerA -> listFlux
+                        .filter(playerB -> playerA.stream()
+                                .anyMatch(a ->  a.club.equals(playerB.club)))
+                )
+                .distinct()
+                .collectMultimap(Player::getClub);
 
+        System.out.println(listFilter.block().size());
+        assert listFilter.block().size() == 1;
+    }
 
+    @Test
+    void reactive_filtrarJugadoresPorPais(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> listFlux = Flux.fromStream(list.parallelStream()).cache();
+        Mono<Map<String, Collection<Player>>> listFilter = listFlux
+                .sort((player1,player2)->Math.max(player1.getWinners(), player2.getWinners()))
+                .distinct()
+                .collectMultimap((player)->player.getNational());
+
+        assert listFilter.block().size() == 164;
+    }
+    
 }
